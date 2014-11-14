4752fecbfd2af5586e47ee41929efa72b50a2e62
BakkerTom <tom.bakker92@gmail.com>
2014-11-14 19:08:49 +0100

    Basics of interface

--- FindMyBike/ViewController.h
+++ FindMyBike/ViewController.h
@@ -7,9 +7,16 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
 
-@interface ViewController : UIViewController
+#define IS_OS_8_OR_LATER ([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0)
+
+@interface ViewController : UIViewController <MKMapViewDelegate, CLLocationManagerDelegate>
+
+@property (strong, nonatomic) IBOutlet MKMapView *mapView;
+@property (weak, nonatomic) IBOutlet UIButton *parkeerButton;
 
 
+- (IBAction)didPressParkeerButton:(id)sender;
 @end
 
--- FindMyBike/ViewController.m
+++ FindMyBike/ViewController.m
@@ -7,21 +7,60 @@
 //
 
 #import "ViewController.h"
+#import <MapKit/MapKit.h>
 
 @interface ViewController ()
+
+@property (strong, nonatomic) CLLocationManager *locationManager;
 
 @end
 
 @implementation ViewController
 
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    _mapView.delegate = self;
+    
+    self.locationManager = [[CLLocationManager alloc] init];
+    self.locationManager.delegate = self;
+    self.locationManager = [[CLLocationManager alloc] init];
+    if(IS_OS_8_OR_LATER) {
+        [self.locationManager requestWhenInUseAuthorization];
+        [self.locationManager requestAlwaysAuthorization];
+    }
+    [self.locationManager startUpdatingLocation];
+    
+    _mapView.showsUserLocation = YES;
+    
+    [_parkeerButton.layer setShadowColor:[UIColor blackColor].CGColor];
+    [_parkeerButton.layer setShadowOpacity:0.2f];
+    [_parkeerButton.layer setShadowRadius:2.0f];
+    [_parkeerButton.layer setShadowOffset:CGSizeMake(0.0, 1.0)];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+//Make the statusBar White
+-(UIStatusBarStyle)preferredStatusBarStyle{
+    return UIStatusBarStyleLightContent;
+}
+
+- (void)mapView:(MKMapView *)mapView
+didUpdateUserLocation:
+(MKUserLocation *)userLocation
+{
+    _mapView.centerCoordinate =
+    userLocation.location.coordinate;
+}
+
+- (IBAction)didPressParkeerButton:(id)sender {
 }
 
 @end
