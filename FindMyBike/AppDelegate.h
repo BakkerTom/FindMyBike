a4825b3e50fd25c124acdda91822b96881aed1f5
Tom Bakker <tom@MacBook-Air-van-Tom-2.local>
2014-11-14 14:56:42 +0100

    Initial Commit

---
+++ FindMyBike/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FindMyBike
+//
+//  Created by Tom Bakker on 14-11-14.
+//  Copyright (c) 2014 Tom Bakker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FindMyBike/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FindMyBike
+//
+//  Created by Tom Bakker on 14-11-14.
+//  Copyright (c) 2014 Tom Bakker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
